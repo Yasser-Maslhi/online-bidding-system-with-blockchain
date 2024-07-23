bc8df46d4015392f0a7f38c95fb125de07cc3b2c
Yasser-Maslhi <Ysr@maslhi@gmail.com>
2024-07-23 13:25:58 +0300

    FE update

--- gp-be/server.js
+++ gp-be/server.js
@@ -56,3 +56,31 @@ process.on("SIGTERM", () => {
     server.close();
   }
 }); 
+//const express = require('express');
+//const mongoose = require('mongoose');
+//const dotenv = require('dotenv');
+//const userRoutes = require('./routes/userRoutes');
+
+//dotenv.config();
+
+//const app = express();
+//app.use(express.json());
+
+//const PORT = process.env.PORT || 5000;
+
+// MongoDB connection
+//mongoose.connect(process.env.MONGO_URI, {
+ // useNewUrlParser: true,
+ // useUnifiedTopology: true
+//}).then(() => {
+ // console.log('Connected to MongoDB');
+//}).catch(err => {
+  //console.log('Failed to connect to MongoDB', err);
+//});
+
+// Routes
+//app.use('/api/users', userRoutes);
+
+//app.listen(PORT, () => {
+  //console.log(`Server is running on port ${PORT}`);
+//});
