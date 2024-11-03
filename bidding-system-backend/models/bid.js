2c02c4346bdaab2e7fb1198cc4230fb56705c8f1
@oalhazmi.c <oalhazmi.c@neoleap.com.sa>
2024-11-03 10:08:28 +0300

    Successfully connected the DB + contract BE server and test the initialize,  placeBid and highestBid routs

--- bidding-system-backend/models/bid.js
+++ bidding-system-backend/models/bid.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
 const bidSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    bidAmount: Number,
-    createdAt: { type: Date, default: Date.now },
+  userId: {
+    type: String,  
+    required: true
+  },
+  bidAmount: {
+    type: Number,
+    required: true
+  },
+  auctionId: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Bid', bidSchema);
